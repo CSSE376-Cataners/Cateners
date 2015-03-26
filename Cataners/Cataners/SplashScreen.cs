effea5478a4605b80178c6c4f6b20bad6907c673
cjm721 <cjmiller721@gmail.com>
2015-03-26 16:30:56 -0400

    Add SplashScreen.cs

---
+++ Cataners/Cataners/SplashScreen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cataners
+{
+    public partial class SplashScreen : Form
+    {
+        public SplashScreen()
+        {
+
+
+            InitializeComponent();
+            this.ClientSize = this.BackgroundImage.Size;
+        }
+
+        private void closeTimer_Tick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
