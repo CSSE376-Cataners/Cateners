80ffbbfdd902b6fb10a98bfaeacda36e3026ce08
cjm721 <cjmiller721@gmail.com>
2015-03-30 17:27:48 -0400

    Added New Shared project. Also start framework setup for client<->server communication

---
+++ Cataners/Cataners/CommunicationClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cataners
+{
+    public class CommunicationClient
+    {
+        System.Net.Sockets.TcpClient clientSocket;
+
+        CommunicationClient()
+        {
+            this.clientSocket = new System.Net.Sockets.TcpClient();
+        }
+
+        public void sendToServer(String msg)
+        {
+
+        }
+    }
+}
