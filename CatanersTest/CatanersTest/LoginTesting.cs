20fa67723deb598b8da4c97c711b823950623f7c
Trent Punt <tpunt3@gmail.com>
2015-03-30 17:38:30 -0400

    created Test code that fails for Verify String #TDD

--- CatanersTest/CatanersTest/LoginTesting.cs
+++ CatanersTest/CatanersTest/LoginTesting.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
-using Rhino.Mocks;
+//using Rhino.Mocks;
+using CatanersShared;
 
 namespace CatanersTest
 {
@@ -17,5 +18,19 @@ namespace CatanersTest
         {
             Assert.True(CatenersServer.ServerMain.testMethod());
         }
+
+        [Test]
+        
+        public void checkStringValid()
+            //testing if string (e.g. username is valid)
+        {
+
+            String text = "username78";
+            Assert.True(Verification.verify(text));
+
+
+        }
+
     }
+
 }
