93559cf0c32bfcab72ce00f9c8b0a5c2bc8eb6f9
cjm721 <cjmiller721@gmail.com>
2015-03-31 11:18:38 -0400

    Create Failing Test Case for CommunicationServer

--- CatanersServer/CatenersServer/CommunicationServer.cs
+++ CatanersServer/CatenersServer/CommunicationServer.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
 
 namespace CatenersServer
 {
+    
     public class CommunicationServer
     {
+        public TcpListener listener;
 
+        public CommunicationServer()
+        {
+
+        }
     }
+
 }
---
+++ CatanersTest/CatanersTest/TestCommunicationServer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CatenersServer;
+
+namespace CatanersTest
+{
+    [TestFixture()]
+    class TestCommunicationServer
+    {
+
+        [Test]
+        public void testTCPIntializes()
+        {
+            CommunicationServer server = new CommunicationServer();
+            Assert.NotNull(server.listener);
+        }
+    }
+}
