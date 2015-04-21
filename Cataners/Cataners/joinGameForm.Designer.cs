b6d40ead2495b7257506acdfa0fb23b48b023549
Trottasn <trottasn@rose-hulman.edu>
2015-04-21 17:56:36 -0400

    JoinGamePopulation (inefficient, but it works)

--- Cataners/Cataners/joinGameForm.Designer.cs
+++ Cataners/Cataners/joinGameForm.Designer.cs
@@ -37,6 +37,8 @@
             this.gameCreatorColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.numberPlayersColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.joinColumn = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.gameTable)).BeginInit();
             this.SuspendLayout();
             // 
@@ -119,12 +121,34 @@
             this.joinColumn.Text = "Join";
             this.joinColumn.Width = 98;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(843, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(843, 257);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // JoinGameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 26F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LawnGreen;
             this.ClientSize = new System.Drawing.Size(947, 503);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.gameTable);
             this.Controls.Add(this.joinGameLabel);
             this.Font = new System.Drawing.Font("Britannic Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -147,5 +171,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn gameCreatorColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn numberPlayersColumn;
         private System.Windows.Forms.DataGridViewButtonColumn joinColumn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Cataners/Cataners/joinGameForm.cs
+++ Cataners/Cataners/joinGameForm.cs
@@ -14,12 +14,31 @@ namespace Cataners
 {
     public partial class JoinGameForm : Form
     {
+        public static JoinGameForm INSTANCE;
+        private BindingSource bs;
         public JoinGameForm()
         {
             InitializeComponent();
-
-            gameTable.DataSource = Data.Lobbies;
+            var bs = new BindingSource();
+            bs.DataSource = Data.Lobbies;
+            gameTable.DataSource = bs;
             CommunicationClient.Instance.sendToServer(new CatanersShared.Message(null, Translation.TYPE.RequestLobbies).toJson());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.bs = new BindingSource();
+            this.bs.DataSource = Data.Lobbies;
+            gameTable.DataSource = bs;
+            gameTable.Show();
+            gameTable.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CommunicationClient.Instance.sendToServer(new CatanersShared.Message(null, Translation.TYPE.RequestLobbies).toJson());
+            bs.DataSource = Data.Lobbies;
+            gameTable.Refresh();
         }
 
     }
