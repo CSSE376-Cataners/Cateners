f2f244df3e0e2b9ff6306dc69b35676096d85ae7
Trottasn <trottasn@rose-hulman.edu>
2015-04-22 19:30:57 -0400

    Hexes in Good Spots and Clicking Hexes

--- CatanersWave/WaveEngineGame/WaveEngineGameProject/MyScene.cs
+++ CatanersWave/WaveEngineGame/WaveEngineGameProject/MyScene.cs
@@ -36,14 +36,45 @@ namespace WaveEngineGameProject
                     X = (WaveServices.Platform.ScreenWidth / 2) - 300
             });
             EntityManager.Add(title);
+
+            var forestHex = new Entity("forestHex")
+                .AddComponent(new Sprite("C:/Users/trottasn/Documents/GitHub/Cateners/CatanersWave/WaveEngineGame/WaveEngineGameProject/BadHex.wpk"))
+                .AddComponent(new SpriteRenderer(DefaultLayers.Alpha))
+                .AddComponent(new Transform2D()
+                {
+                    Y = (WaveServices.Platform.ScreenHeight / 2) - 126,
+                    X = (WaveServices.Platform.ScreenWidth / 2) - 110
+                });
+            EntityManager.Add(forestHex);
+            var forestHex2 = new Entity("forestHex2")
+                .AddComponent(new Sprite("C:/Users/trottasn/Documents/GitHub/Cateners/CatanersWave/WaveEngineGame/WaveEngineGameProject/BadHex.wpk"))
+                .AddComponent(new SpriteRenderer(DefaultLayers.Alpha))
+                .AddComponent(new Transform2D()
+                {
+                    Y = (WaveServices.Platform.ScreenHeight / 2) + 65,
+                    X = (WaveServices.Platform.ScreenWidth / 2)
+                });
+            EntityManager.Add(forestHex2);
+            var forestHex3 = new Entity("forestHex3")
+                .AddComponent(new Sprite("C:/Users/trottasn/Documents/GitHub/Cateners/CatanersWave/WaveEngineGame/WaveEngineGameProject/BadHex.wpk"))
+                .AddComponent(new SpriteRenderer(DefaultLayers.Alpha))
+                .AddComponent(new RectangleCollider())
+                .AddComponent(new TouchGestures())
+                .AddComponent(new Transform2D()
+                {
+                    Y = (WaveServices.Platform.ScreenHeight / 2) - 126,
+                    X = (WaveServices.Platform.ScreenWidth / 2) + 110
+                });
+            forestHex3.FindComponent<TouchGestures>().TouchPressed += new EventHandler<GestureEventArgs>(forestHex3_TouchPressed);
+            EntityManager.Add(forestHex3);
             #endregion
         }
 
     
     
-        void temp_TouchTap(object sender, GestureEventArgs e)
+        private void forestHex3_TouchPressed(object sender, GestureEventArgs e)
         {
-            Console.WriteLine("I BEEN TOUCHED");
+            Console.WriteLine("Hex 3 Touched : Right Hex");
         }
 
         protected override void Start()
