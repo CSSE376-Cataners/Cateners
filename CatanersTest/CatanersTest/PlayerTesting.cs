7d714b2433075965db02a415f2f0951b10771ea1
cjm721 <cjmiller721@gmail.com>
2015-04-23 16:54:03 -0400

    Player Regresion Tests

---
+++ CatanersTest/CatanersTest/PlayerTesting.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CatanersShared;
+
+
+namespace CatanersTest
+{
+    [TestFixture()]
+    class PlayerTesting
+    {
+
+        [Test]
+        public void testInstanciation()
+        {
+            Player a = new Player("Test");
+
+            Assert.NotNull(a);
+        }
+
+        [Test]
+        public void testPlayerReturn()
+        {
+            Player a = new Player("abc123");
+
+            Assert.AreEqual("abc123", a.Username);
+        }
+
+        [Test]
+        public void testPlayerEqual()
+        {
+            Player a = new Player("abc123");
+            Player b = new Player("abc123");
+
+            Assert.AreEqual(a, b);
+        }
+
+    }
+}
