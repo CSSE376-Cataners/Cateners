b82c27be472afe4581dfff739ce4cdb0f7c44c78
cjm721 <cjmiller721@gmail.com>
2015-04-27 18:29:52 -0400

    ServerPlayer Constructor Test

---
+++ CatanersTest/CatanersTest/ServerPlayerTesting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatenersServer;
+using NUnit.Framework;
+
+namespace CatanersTest
+{
+    [TestFixture()]
+    public class ServerPlayerTesting
+    {
+
+        [Test]
+        public void testConstructor()
+        {
+            Client client = new Client();
+            String username = "UserName";
+            ServerPlayer player = new ServerPlayer(username,client);
+
+            Assert.AreEqual(client, player.client);
+            Assert.AreEqual(username, player.Username);
+
+
+        }
+    }
+}
