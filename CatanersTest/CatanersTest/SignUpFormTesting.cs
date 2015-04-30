5a7a0a8113d5d9c0ee13bd6e23a87847a6f53729
cjm721 <cjmiller721@gmail.com>
2015-04-30 16:55:07 -0400

    SignUpForm Testing

---
+++ CatanersTest/CatanersTest/SignUpFormTesting.cs
@@ -0,0 +1,22 @@
+﻿using Cataners;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatanersTest
+{
+    [TestFixture()]
+    public class SignUpFormTesting
+    {
+        [Test]
+        public void testContructor()
+        {
+            SignUpForm suf = new SignUpForm();
+
+            Assert.NotNull(suf);
+        }
+    }
+}
