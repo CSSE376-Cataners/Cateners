f104ceca6385514338a92ce72ffdb061136778a0
cjm721 <cjmiller721@gmail.com>
2015-04-29 21:19:39 -0400

    Write tests for Lobby.GetHashCode()

--- CatanersTest/CatanersTest/TestTranslation.cs
+++ CatanersTest/CatanersTest/TestTranslation.cs
@@ -4,16 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using CatanersShared;
 
 namespace CatanersTest
 {
     [TestFixture]
     class TestTranslation
     {
-
-        public void testLoginTranslate()
+        [Test]
+        public void testTypeInializes()
         {
-            Assert.AreEqual("LOGIN: ","");
+            new Translation();
+            foreach (Translation.TYPE t in Enum.GetValues(typeof(Translation.TYPE))) { }
+
+            Assert.Pass();
         }
     }
 }
