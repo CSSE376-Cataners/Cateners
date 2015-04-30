4ddd1b9851818a1683057c13987b9013bbf93087
Trent Punt <tpunt3@gmail.com>
2015-04-30 15:54:21 -0400

    added data to waveengineGameProject and removed it from Cataners

--- CatanersWave/WaveEngineGame/WaveEngineGameProject/Data.cs
+++ CatanersWave/WaveEngineGame/WaveEngineGameProject/Data.cs
@@ -1,11 +1,12 @@
-﻿using CatanersShared;
+﻿using WaveEngineGameProject;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CatanersShared;
 
-namespace Cataners
+namespace WaveEngineGameProject
 {
     public class Data
     {
