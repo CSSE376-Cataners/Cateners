8c5e81070f9faad1acdc78fee60d4ea29b0ad04d
cjm721 <cjmiller721@gmail.com>
2015-04-30 16:54:19 -0400

    New MainGui Tests

---
+++ CatanersTest/CatanersTest/MainGuiTesting.cs
@@ -0,0 +1,26 @@
+﻿using Cataners;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatanersTest
+{
+    [TestFixture()]
+    public class MainGuiTesting
+    {
+
+        [Test]
+        public void testConstructor()
+        {
+            MainGui gui = new MainGui();
+
+            Assert.NotNull(gui);
+            Assert.NotNull(MainGui.INSTANCE);
+            Assert.NotNull(CommunicationClient.Instance);
+
+        }
+    }
+}
