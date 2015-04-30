860c96a4f02d8e7eb0ccb27af271672af15597a2
Trent Punt <tpunt3@gmail.com>
2015-04-30 17:49:17 -0400

    added gamePlayer

---
+++ CatanersWave/WaveEngineGame/WaveEngineGameProject/GamePlayer.cs
@@ -0,0 +1,22 @@
+﻿using CatanersShared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaveEngineGameProject
+{
+    class GamePlayer : Player
+    {
+        public Dictionary<Resource.TYPE, int> resources = new Dictionary<Resource.TYPE, int>();
+   
+        public GamePlayer(String username) : base(username)
+        {
+            
+        }
+
+    }
+
+    
+}
