3347936ba71fd5d889f457118c299a1fbdee0ed9
Trent Punt <tpunt3@gmail.com>
2015-05-01 00:23:30 -0400

    added constructor test

---
+++ CatanersTest/CatanersTest/GameLobbyTesting.cs
@@ -0,0 +1,31 @@
+﻿using CatanersShared;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatanersTest
+{
+    [TestFixture]
+    class GameLobbyTesting
+    {
+        
+        
+    [Test]
+    public void TestConstructor(){
+        Lobby lobby = new Lobby("lobby", 100, new Player("LichKing"), 33);
+        lobby.addPlayer(new Player("Arthus"));
+
+        GameLobby gameLobby = new GameLobby(lobby);
+        Assert.AreEqual(gameLobby.GameName, lobby.GameName);
+        Assert.AreEqual(gameLobby.MaxTimePerTurn, lobby.MaxTimePerTurn);
+        Assert.AreEqual(gameLobby.Owner, lobby.Owner);
+        Assert.AreEqual(gameLobby.lobbyID, lobby.lobbyID);
+        Assert.AreEqual(gameLobby.Players, lobby.Players);
+    }
+
+    
+
+}
