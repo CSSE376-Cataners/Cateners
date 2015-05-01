392da5f7e9f35ea4655030e877aeca3cc3e53e52
Trent Punt <tpunt3@gmail.com>
2015-05-01 00:36:28 -0400

    added code to fix test GamePlayer TDD

---
+++ CatanersShared/CatanersShared/GamePlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatanersShared
+{
+    public class GamePlayer : Player
+    {
+        private int victoryPoints;
+        private bool isMyTurn;
+        //private Dictionary resources
+        public GamePlayer(Player player)
+            : base(player.Username)
+        {
+
+        }
+
+    }
+}
