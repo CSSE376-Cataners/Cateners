48a0cff3bda187ebe59323bdbf9c0076291b66dd
cjm721 <cjmiller721@gmail.com>
2015-05-03 18:41:40 -0400

    Passes Tests

---
+++ CatanersShared/CatanersShared/Chat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatanersShared
+{
+    public class Chat
+    {
+
+        public enum TYPE {Normal, Private};
+
+
+        public TYPE ChatType;
+        public String Message;
+        public object Special;
+
+
+        public Chat()
+        {
+            // For Json pasrsing 
+        }
+
+        public Chat(String message, TYPE y, Object Special)
+        {
+            this.Message = message;
+            this.ChatType = y;
+            this.Special = Special;
+        }
+    }
+}
