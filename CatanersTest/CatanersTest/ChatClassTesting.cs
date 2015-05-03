cd1340da6f4109ab603f1a8f0a7c9e415257e8c2
cjm721 <cjmiller721@gmail.com>
2015-05-03 18:33:57 -0400

    Tests for a new Class

---
+++ CatanersTest/CatanersTest/ChatClassTesting.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatanersShared;
+
+namespace CatanersTest
+{
+
+    [TestFixture()]
+    public class ChatClassTesting
+    {
+
+        [Test]
+        public void testConstructor()
+        {
+            Chat chat = new Chat("Message", TYPE.Normal, "Reciver");
+        }
+    }
+}
