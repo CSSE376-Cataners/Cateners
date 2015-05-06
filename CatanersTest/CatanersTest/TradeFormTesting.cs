171daa13bf5d1777b05f42c526b694d0c4d3ac20
Trent Punt <tpunt3@gmail.com>
2015-05-05 23:19:59 -0400

    wrote Test for Adding Players to TradeForm

---
+++ CatanersTest/CatanersTest/TradeFormTesting.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Mocks;
+using Cataners;
+using CatanersShared;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace CatanersTest
+{
+    [TestFixture()]
+    class TradeFormTesting
+    {
+        TradeForm trade;
+        [SetUp]
+        public void setUp()
+        {
+            trade = new TradeForm();
+        }
+
+       [Test]
+        public void TestThatPlayerAddedInComboBox()
+        {
+           Player p1 = new Player("Bobby Tables");
+           GameLobby lobby = new GameLobby(new Lobby("game", 100, p1, 10));
+           Data.currentLobby = lobby;
+           GamePlayer gp1 = new GamePlayer("Bobby Tables");
+           trade.initializeValues(gp1);
+           BindingFlags flags = BindingFlags.Instance | BindingFlags.NonPublic;
+           FieldInfo info = (typeof(TradeForm).GetField("targetOfTradeComboBox", flags));
+           ComboBox box = (ComboBox)info.GetValue(trade);
+           
+
+           Assert.AreEqual(p1.Username, box.Items[0]);
+
+        }
+
+        [Test]
+       public void TestBrickCheck()
+       {
+           
+       }
+        
+    }
+}
