2c63a97700a8b74b01adf5e6091bd42b1f105f31
cjm721 <cjmiller721@gmail.com>
2015-05-06 00:56:00 -0400

    MCDC for GameLobby Equals

--- CatanersTest/CatanersTest/GameLobbyTesting.cs
+++ CatanersTest/CatanersTest/GameLobbyTesting.cs
@@ -11,55 +11,97 @@ namespace CatanersTest
     [TestFixture]
     class GameLobbyTesting
     {
-        
-        
-    [Test]
-    public void TestConstructor(){
-        Lobby lobby = new Lobby("lobby", 100, new Player("LichKing"), 33);
-        lobby.addPlayer(new Player("Arthus"));
 
-        GameLobby gameLobby = new GameLobby(lobby);
-        Assert.AreEqual(gameLobby.GameName, lobby.GameName);
-        Assert.AreEqual(gameLobby.MaxTimePerTurn, lobby.MaxTimePerTurn);
-        Assert.AreEqual(gameLobby.Owner, lobby.Owner);
-        Assert.AreEqual(gameLobby.lobbyID, lobby.lobbyID);
-        Assert.AreEqual(gameLobby.gamePlayers.Count, lobby.Players.Count);
-    }
 
-    [Test]
-    public void TestThatAllPlayersGetAddedToGameLobby()
-    {
-        Lobby lobby = new Lobby("You Are Not Prepared", 100, new Player("Arthus"), 33);
-        lobby.addPlayer(new Player("LadySylvanas"));
-        lobby.addPlayer(new Player("Illidan"));
-
-        GameLobby gameLobby = new GameLobby(lobby);
-
-        for (int i = 0; i < lobby.PlayerCount; i++)
+        [Test]
+        public void TestConstructor()
         {
-            Assert.AreEqual(gameLobby.gamePlayers[i], lobby.Players[i]);
+            Lobby lobby = new Lobby("lobby", 100, new Player("LichKing"), 33);
+            lobby.addPlayer(new Player("Arthus"));
+
+            GameLobby gameLobby = new GameLobby(lobby);
+            Assert.AreEqual(gameLobby.GameName, lobby.GameName);
+            Assert.AreEqual(gameLobby.MaxTimePerTurn, lobby.MaxTimePerTurn);
+            Assert.AreEqual(gameLobby.Owner, lobby.Owner);
+            Assert.AreEqual(gameLobby.lobbyID, lobby.lobbyID);
+            Assert.AreEqual(gameLobby.gamePlayers.Count, lobby.Players.Count);
         }
-    }
 
-    [Test]
-    public void testGameLobbyToJson()
-    {
-        Lobby newLobby = new Lobby("Test", 3, new Player("Gandalf"), 2);
-        GameLobby gamelobby = new GameLobby(newLobby);
-        Console.WriteLine(gamelobby.toJson());
-        Assert.AreEqual("{\"gamePlayers\":[{\"resources\":{\"Brick\":0,\"Ore\":0,\"Sheep\":0,\"Wheat\":0,\"Wood\":0},\"Ready\":false,\"Username\":\"Gandalf\"}],\"lobbyID\":2,\"allReady\":false,\"Players\":[{\"resources\":{\"Brick\":0,\"Ore\":0,\"Sheep\":0,\"Wheat\":0,\"Wood\":0},\"Ready\":false,\"Username\":\"Gandalf\"}],\"GameName\":\"Test\",\"MaxTimePerTurn\":3,\"Owner\":{\"resources\":{\"Brick\":0,\"Ore\":0,\"Sheep\":0,\"Wheat\":0,\"Wood\":0},\"Ready\":false,\"Username\":\"Gandalf\"},\"PlayerCount\":1}", gamelobby.toJson());
-    }
+        [Test]
+        public void TestThatAllPlayersGetAddedToGameLobby()
+        {
+            Lobby lobby = new Lobby("You Are Not Prepared", 100, new Player("Arthus"), 33);
+            lobby.addPlayer(new Player("LadySylvanas"));
+            lobby.addPlayer(new Player("Illidan"));
 
-    [Test]
-    public void testGameLobbyFromJson()
-    {
-        Lobby testLobby = new Lobby("Test", 3, new Player("Saruman"), 2);
-        GameLobby gamelobby = new GameLobby(testLobby);
-        Console.WriteLine(gamelobby);
-        Lobby newLobby = Lobby.fromJson("{\"Players\":[{\"resources\":{\"Brick\":0,\"Ore\":0,\"Sheep\":0,\"Wheat\":0,\"Wood\":0},\"Ready\":false,\"Username\":\"Saruman\"}],\"lobbyID\":2,\"allReady\":false,\"GameName\":\"Test\",\"MaxTimePerTurn\":3,\"Owner\":{\"resources\":{\"Brick\":0,\"Ore\":0,\"Sheep\":0,\"Wheat\":0,\"Wood\":0},\"Ready\":false,\"Username\":\"Saruman\"},\"PlayerCount\":1}");
-        Console.WriteLine(newLobby);
-        Assert.True(testLobby.Equals(newLobby));
-    }
+            GameLobby gameLobby = new GameLobby(lobby);
+
+            for (int i = 0; i < lobby.PlayerCount; i++)
+            {
+                Assert.AreEqual(gameLobby.gamePlayers[i], lobby.Players[i]);
+            }
+        }
+
+        [Test]
+        public void testGameLobbyToJson()
+        {
+            Lobby newLobby = new Lobby("Test", 3, new Player("Gandalf"), 2);
+            GameLobby gamelobby = new GameLobby(newLobby);
+            Console.WriteLine(gamelobby.toJson());
+            Assert.AreEqual("{\"gamePlayers\":[{\"resources\":{\"Brick\":0,\"Ore\":0,\"Sheep\":0,\"Wheat\":0,\"Wood\":0},\"Ready\":false,\"Username\":\"Gandalf\"}],\"lobbyID\":2,\"allReady\":false,\"Players\":[{\"resources\":{\"Brick\":0,\"Ore\":0,\"Sheep\":0,\"Wheat\":0,\"Wood\":0},\"Ready\":false,\"Username\":\"Gandalf\"}],\"GameName\":\"Test\",\"MaxTimePerTurn\":3,\"Owner\":{\"resources\":{\"Brick\":0,\"Ore\":0,\"Sheep\":0,\"Wheat\":0,\"Wood\":0},\"Ready\":false,\"Username\":\"Gandalf\"},\"PlayerCount\":1}", gamelobby.toJson());
+        }
+
+        [Test]
+        public void testGameLobbyFromJson()
+        {
+            Lobby testLobby = new Lobby("Test", 3, new Player("Saruman"), 2);
+            GameLobby gamelobby = new GameLobby(testLobby);
+            Console.WriteLine(gamelobby);
+            Lobby newLobby = Lobby.fromJson("{\"Players\":[{\"resources\":{\"Brick\":0,\"Ore\":0,\"Sheep\":0,\"Wheat\":0,\"Wood\":0},\"Ready\":false,\"Username\":\"Saruman\"}],\"lobbyID\":2,\"allReady\":false,\"GameName\":\"Test\",\"MaxTimePerTurn\":3,\"Owner\":{\"resources\":{\"Brick\":0,\"Ore\":0,\"Sheep\":0,\"Wheat\":0,\"Wood\":0},\"Ready\":false,\"Username\":\"Saruman\"},\"PlayerCount\":1}");
+            Console.WriteLine(newLobby);
+            Assert.True(testLobby.Equals(newLobby));
+        }
+
+        [Test]
+        public void testEquals()
+        {
+            Lobby lobboWithPlayers = new Lobby("GameName", 10, new Player("Owner"), 1);
+            GameLobby lobby1 = new GameLobby(lobboWithPlayers);
+            GameLobby lobby2 = new GameLobby(lobboWithPlayers);
+
+            Assert.True(lobby1.Equals(lobby2));
+
+            lobboWithPlayers.addPlayer(new Player("Bob"));
+            lobboWithPlayers.addPlayer(new Player("Bobby"));
+            lobboWithPlayers.addPlayer(new Player("Robert"));
+
+            lobby1 = new GameLobby(lobboWithPlayers);
+            lobby2 = new GameLobby(lobboWithPlayers);
+
+            Assert.True(lobby1.Equals(lobby2));
+
+            lobby2.gamePlayers.Add(new GamePlayer("YoYo"));
+
+            Assert.False(lobby1.Equals(lobby2));
+
+            Assert.False(lobby1.Equals(null));
+            Assert.False(lobby1.Equals(""));
+
+
+            lobby1 = new GameLobby(lobboWithPlayers);
+            lobby2 = new GameLobby(lobboWithPlayers);
+
+            lobby2.Owner = new GamePlayer("Not you");
+            Assert.False(lobby1.Equals(lobby2));
+            
+            lobby2 = new GameLobby(lobboWithPlayers);
+            lobby2.MaxTimePerTurn = -1;
+            Assert.False(lobby1.Equals(lobby2));
+
+            lobby2 = new GameLobby(lobboWithPlayers);
+            lobby2.GameName = "IDK";
+            Assert.False(lobby1.Equals(lobby2));
+        }
 
     }
 
