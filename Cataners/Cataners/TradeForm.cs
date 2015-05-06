9c9d76602d230f12d1f2afccab973f958aa0e538
Trent Punt <tpunt3@gmail.com>
2015-05-05 23:25:19 -0400

    wrote adding Players to TradeForm TDD

--- Cataners/Cataners/TradeForm.cs
+++ Cataners/Cataners/TradeForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CatanersShared;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,10 +14,30 @@ namespace Cataners
     public partial class TradeForm : Form
     {
         public static TradeForm INSTANCE;
+        private GamePlayer currentTrader;
+        private bool brickCheck;
         public TradeForm()
         {
             InitializeComponent();
             INSTANCE = this;
+            currentTrader = null;
+        }
+
+        public void initializeValues(GamePlayer user){
+            //initialize players in the target box
+            for (int i = 0; i < ((GameLobby)Data.currentLobby).gamePlayers.Count; i++ ) {
+                targetOfTradeComboBox.Items.Add(((GameLobby)Data.currentLobby).gamePlayers[i].Username);
+            }
+            currentTrader = user;
+        }
+
+        private void giveBrickTextBox_TextChanged(object sender, EventArgs e)
+        {
+            CheckBrickQuantity();
+        }
+
+        private void CheckBrickQuantity()
+        {
         }
 
     }
