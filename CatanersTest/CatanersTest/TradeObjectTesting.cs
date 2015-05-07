2216e592e287fb74634de5e9d7524211b40135f5
cjm721 <cjmiller721@gmail.com>
2015-05-07 11:14:23 -0400

    ToJsonTest

--- CatanersTest/CatanersTest/TradeObjectTesting.cs
+++ CatanersTest/CatanersTest/TradeObjectTesting.cs
@@ -26,6 +26,30 @@ namespace CatanersTest
             Assert.IsNotNull(tradeobj.wantedResources);
         }
 
+        [Test]
+        public void testToJson()
+        {
+            Dictionary<Resource.TYPE,int> offer = new Dictionary<Resource.TYPE,int>();
+            Dictionary<Resource.TYPE,int> request = new Dictionary<Resource.TYPE,int>();
+
+            int x = 0;
+            foreach (Resource.TYPE t in Enum.GetValues(typeof(Resource.TYPE)))
+            {
+                offer[t] = 10 + x;
+                request[t] = 10 + x++;
+            }
+
+            Trade trade = new Trade(new GamePlayer("Sender"), new GamePlayer("Reciver"), offer, request);
+
+            String json = trade.toJson();
+
+            Console.WriteLine(json);
+
+            Assert.AreEqual("{\"source\":{\"resources\":{\"Brick\":0,\"Ore\":0,\"Sheep\":0,\"Wheat\":0,\"Wood\":0},\"Ready\":false,\"Username\":\"Sender\"},\"target\":{\"resources\":{\"Brick\":0,\"Ore\":0,\"Sheep\":0,\"Wheat\":0,\"Wood\":0},\"Ready\":false,\"Username\":\"Reciver\"},\"offeredResources\":{\"Wheat\":10,\"Sheep\":11,\"Brick\":12,\"Ore\":13,\"Wood\":14},\"wantedResources\":{\"Wheat\":10,\"Sheep\":11,\"Brick\":12,\"Ore\":13,\"Wood\":14}}", json);
+        }
+
+
+
 
     }
 }
