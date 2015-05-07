aa9127a6efa1442a02bb0c474305df9273c4fa43
cjm721 <cjmiller721@gmail.com>
2015-05-07 14:51:49 -0400

    Write Equals

--- CatanersShared/CatanersShared/Trade.cs
+++ CatanersShared/CatanersShared/Trade.cs
@@ -32,21 +32,13 @@ namespace CatanersShared
         // override object.Equals
         public override bool Equals(object obj)
         {
-            //       
-            // See the full list of guidelines at
-            //   http://go.microsoft.com/fwlink/?LinkID=85237  
-            // and also the guidance for operator== at
-            //   http://go.microsoft.com/fwlink/?LinkId=85238
-            //
-
-            if (obj == null || GetType() != obj.GetType())
+            if (obj == null || !(obj is Trade))
             {
                 return false;
             }
 
-            // TODO: write your implementation of Equals() here
-            throw new NotImplementedException();
-            return base.Equals(obj);
+            Trade objt = (Trade)obj;
+            return this.GetHashCode() == objt.GetHashCode();
         }
 
         // override object.GetHashCode
