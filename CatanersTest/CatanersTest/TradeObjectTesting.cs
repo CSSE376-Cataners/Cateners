6785fce148a77eac469835f845fab1354aa79cd0
cjm721 <cjmiller721@gmail.com>
2015-05-07 11:28:05 -0400

    Equals Test

--- CatanersTest/CatanersTest/TradeObjectTesting.cs
+++ CatanersTest/CatanersTest/TradeObjectTesting.cs
@@ -48,7 +48,69 @@ namespace CatanersTest
             Assert.AreEqual("{\"source\":{\"resources\":{\"Brick\":0,\"Ore\":0,\"Sheep\":0,\"Wheat\":0,\"Wood\":0},\"Ready\":false,\"Username\":\"Sender\"},\"target\":{\"resources\":{\"Brick\":0,\"Ore\":0,\"Sheep\":0,\"Wheat\":0,\"Wood\":0},\"Ready\":false,\"Username\":\"Reciver\"},\"offeredResources\":{\"Wheat\":10,\"Sheep\":11,\"Brick\":12,\"Ore\":13,\"Wood\":14},\"wantedResources\":{\"Wheat\":10,\"Sheep\":11,\"Brick\":12,\"Ore\":13,\"Wood\":14}}", json);
         }
 
+        [Test]
+        public void testFromJson()
+        {
 
+        }
+
+        [Test]
+        public void testEquals()
+        {
+            Dictionary<Resource.TYPE, int> offer = new Dictionary<Resource.TYPE, int>();
+            Dictionary<Resource.TYPE, int> request = new Dictionary<Resource.TYPE, int>();
+
+            int x = 0;
+            foreach (Resource.TYPE t in Enum.GetValues(typeof(Resource.TYPE)))
+            {
+                offer[t] = 10 + x;
+                request[t] = 10 + x++;
+            }
+
+            Trade trade1 = new Trade(new GamePlayer("Sender"), new GamePlayer("Reciver"), offer, request);
+
+            Dictionary<Resource.TYPE, int> offer2 = new Dictionary<Resource.TYPE, int>();
+            Dictionary<Resource.TYPE, int> request2 = new Dictionary<Resource.TYPE, int>();
+
+            x = 5;
+            foreach (Resource.TYPE t in Enum.GetValues(typeof(Resource.TYPE)))
+            {
+                offer2[t] = 10 + x;
+                request2[t] = 10 + x++;
+            }
+
+            Trade trade = new Trade(new GamePlayer("Sender"), new GamePlayer("Reciver"), offer2, request2);
+
+            Dictionary<Resource.TYPE, int> offera = new Dictionary<Resource.TYPE, int>();
+            Dictionary<Resource.TYPE, int> requesta = new Dictionary<Resource.TYPE, int>();
+
+            x = 0;
+            foreach (Resource.TYPE t in Enum.GetValues(typeof(Resource.TYPE)))
+            {
+                offera[t] = 10 + x;
+                requesta[t] = 10 + x++;
+            }
+
+            Trade tradea = new Trade(new GamePlayer("Sender"), new GamePlayer("Reciver"), offer, request);
+
+
+            Assert.True(trade.Equals(trade));
+            Assert.True(trade.Equals(tradea));
+            Assert.False(trade.Equals(trade1));
+            Assert.False(trade.Equals(null));
+            Assert.False(trade.Equals(""));
+
+
+            Trade trade3 = new Trade(new GamePlayer("Diffrent"), new GamePlayer("Reciver"), offer, request);
+            Trade trade4 = new Trade(new GamePlayer("Sender"), new GamePlayer("Diffrent"), offer, request);
+
+
+            Assert.False(trade.Equals(trade3));
+            Assert.False(trade.Equals(trade4));
+
+
+
+        }
 
 
     }
