407c1894e7fa412d61ab4973e9a7f7ea8e74e188
Trent Punt <tpunt3@gmail.com>
2015-05-07 01:07:26 -0400

    wrote test for TradeObject

---
+++ CatanersTest/CatanersTest/TradeObjectTesting.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatanersShared;
+
+namespace CatanersTest
+{
+    [TestFixture]
+    class TradeObjectTesting
+    {
+        [Test]
+        public void TestConstrutor()
+        {
+            GamePlayer source = new GamePlayer("source");
+            GamePlayer target = new GamePlayer("target");
+            Dictionary<Resource.TYPE,int> offered = new Dictionary<Resource.TYPE,int>();
+            Dictionary<Resource.TYPE,int> wanted = new Dictionary<Resource.TYPE,int>();
+
+            Trade tradeobj = new Trade(source, target, offered, wanted);
+            Assert.IsNotNull(tradeobj.source);
+            Assert.IsNotNull(tradeobj.target);
+            Assert.IsNotNull(tradeobj.offeredResources);
+            Assert.IsNotNull(tradeobj.wantedResources);
+        }
+
+
+    }
+}
