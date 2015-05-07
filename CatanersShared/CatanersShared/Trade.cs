462aa12de4ec48c67df102a7950c68bcd3e1d2ea
Trent Punt <tpunt3@gmail.com>
2015-05-07 00:34:04 -0400

    added trade class

---
+++ CatanersShared/CatanersShared/Trade.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatanersShared
+{
+    class Trade
+    {
+
+        public GamePlayer source;
+        public GamePlayer target;
+        public Dictionary<Resource.TYPE, int> offeredResources;
+        public Dictionary<Resource.TYPE, int> wantedResources;
+
+        public Trade(GamePlayer src, GamePlayer tar, Dictionary<Resource.TYPE,int> offered, Dictionary<Resource.TYPE,int> wanted)
+        {
+            this.source = src;
+            this.target = tar;
+            this.offeredResources = offered;
+            this.wantedResources = wanted;
+
+        }
+
+    }
+
+    
+}
