d1b131f43a7644ffffc2ef5042ba60ec677b6855
Trent Punt <tpunt3@gmail.com>
2015-05-07 21:49:56 -0400

    framework for PopUpMessage + testing

---
+++ CatanersShared/CatanersShared/PopUpMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatanersShared
+{
+    class PopUpMessage
+    {
+        public enum TYPE { Notification, ResponseNeeded };
+        PopUpMessage.TYPE type;
+        String titleMsg;
+        String bodyMsg;
+
+        public PopUpMessage(String titleMessage, String bodyMessage, PopUpMessage.TYPE type)
+        {
+            
+
+
+        }
+
+    }
+}
---
+++ CatanersTest/CatanersTest/PopUpMessageTesting.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatanersTest
+{
+
+    [TestFixture]
+    public class PopUpMessageTesting
+    {
+
+        [Test]
+        public void PopUpMessageTestInitialize(){
+
+        }
+    }
+}
