b09760db8bd7d650e8abdecc0e293ff8c88aa0f1
Trent Punt <tpunt3@gmail.com>
2015-05-07 23:08:16 -0400

    test initialization of addresource TDD

---
+++ CatanersTest/CatanersTest/ResourceTesting.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatanersShared;
+
+namespace CatanersTest
+{
+    [TestFixture]
+    class ResourceTesting
+    {
+
+        [Test]
+        public void ResourceTestInitialize()
+        {
+            Player player = new Player("Bobby Tables");
+            AddResource resource = new AddResource(player, Resource.TYPE.Brick,3);
+
+            Assert.AreEqual(player, resource.player);
+            Assert.AreEqual(Resource.TYPE.Brick, resource.resourceType);
+            Assert.AreEqual(3, resource.number);
+
+         
+
+        }
+
+    }
+}
