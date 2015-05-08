3154f0810eb9e8f7ca74a510203a855b55a3f2d2
Trent Punt <tpunt3@gmail.com>
2015-05-07 23:07:09 -0400

    from Json Popup Test TDD

--- CatanersTest/CatanersTest/PopUpMessageTesting.cs
+++ CatanersTest/CatanersTest/PopUpMessageTesting.cs
@@ -34,6 +34,15 @@ namespace CatanersTest
 
         }
 
+        [Test]
+        public void PopUpMessageTestFromJson()
+        {
+
+            PopUpMessage popup = new PopUpMessage("Title", "Body", PopUpMessage.TYPE.Notification);
+            PopUpMessage popup2 = PopUpMessage.fromJson("{\"type\":0,\"titleMsg\":\"Title\",\"bodyMsg\":\"Body\"}");
+            Assert.AreEqual(popup, popup2);
+
+        }
 
     }
 }
