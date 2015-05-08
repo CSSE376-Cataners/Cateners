9a9ef5128bb7a553c8340329a13c658b2f8ceb59
Trent Punt <tpunt3@gmail.com>
2015-05-07 23:06:43 -0400

    wrote code to fix test popupmessage TDD

--- CatanersShared/CatanersShared/PopUpMessage.cs
+++ CatanersShared/CatanersShared/PopUpMessage.cs
@@ -20,8 +20,22 @@ namespace CatanersShared
             this.bodyMsg = bodyMessage;
             this.type = type;
 
-
         }
 
+        public String toJson(){
+            return Newtonsoft.Json.JsonConvert.SerializeObject(this);
+        }
+       
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is PopUpMessage))
+            {
+                return false;
+            }
+
+            PopUpMessage other = (PopUpMessage)obj;
+
+            return this.bodyMsg.Equals(other.bodyMsg) && this.titleMsg.Equals(other.titleMsg) && this.type == other.type; 
+        }
     }
 }
