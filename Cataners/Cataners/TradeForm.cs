23c48786627db763fceb066115b1b7488721b560
Trent Punt <tpunt3@gmail.com>
2015-05-10 13:26:57 -0400

    wrote code to trade wheat with bank TDD

--- Cataners/Cataners/TradeForm.cs
+++ Cataners/Cataners/TradeForm.cs
@@ -462,6 +462,10 @@ namespace Cataners
         {
             return (offeredBrick == 0 && offeredOre == 0 && offeredSheep == 4 && offeredWheat == 0 && offeredWood == 0);
         }
+        public bool bankCheckWheat()
+        {
+            return (offeredBrick == 0 && offeredOre == 0 && offeredSheep == 0 && offeredWheat == 4 && offeredWood == 0);
+        }
 
 
     }
