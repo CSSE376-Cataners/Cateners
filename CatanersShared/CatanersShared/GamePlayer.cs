1daf817097a8d8b5b2850b0322c6ba7a6878e50b
Trent Punt <tpunt3@gmail.com>
2015-05-11 17:33:37 -0400

    made players start with 0 resources

--- CatanersShared/CatanersShared/GamePlayer.cs
+++ CatanersShared/CatanersShared/GamePlayer.cs
@@ -18,11 +18,11 @@ namespace CatanersShared
             : base(Username)
         {
             resources = new Dictionary<Resource.TYPE, int>();
-            resources.Add(Resource.TYPE.Brick, 10);
-            resources.Add(Resource.TYPE.Ore, 10);
-            resources.Add(Resource.TYPE.Sheep, 10);
-            resources.Add(Resource.TYPE.Wheat, 10);
-            resources.Add(Resource.TYPE.Wood, 10);
+            resources.Add(Resource.TYPE.Brick, 0);
+            resources.Add(Resource.TYPE.Ore, 0);
+            resources.Add(Resource.TYPE.Sheep, 0);
+            resources.Add(Resource.TYPE.Wheat, 0);
+            resources.Add(Resource.TYPE.Wood, 0);
 
             resourceCount = resources[Resource.TYPE.Brick] + resources[Resource.TYPE.Ore] + resources[Resource.TYPE.Sheep] + resources[Resource.TYPE.Wheat] + resources[Resource.TYPE.Wood]; 
         }
