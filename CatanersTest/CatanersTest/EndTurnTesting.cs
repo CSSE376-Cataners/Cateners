bfc9eee1fe431ae61cc84c09f57e39401a54f451
cjm721 <cjmiller721@gmail.com>
2015-05-14 22:21:18 -0400

    Test for constructor

---
+++ CatanersTest/CatanersTest/EndTurnTesting.cs
@@ -0,0 +1,25 @@
+﻿using CatanersShared;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatanersTest
+{
+    [TestFixture()]
+    public class EndTurnTesting
+    {
+
+        [Test]
+        public void testToJson()
+        {
+            EndTurn et = new EndTurn(1, EndTurn.Phase.GamePhase);
+
+            Assert.AreEqual(1, et.playerTurn);
+            Assert.AreEqual(EndTurn.Phase.GamePhase, et.phase);
+
+        }
+    }
+}
