3ac7a75534b647c2e802fe8c1a7c53379a5557b7
cjm721 <cjmiller721@gmail.com>
2015-05-14 22:30:33 -0400

    Write  to json Test

--- CatanersTest/CatanersTest/EndTurnTesting.cs
+++ CatanersTest/CatanersTest/EndTurnTesting.cs
@@ -20,5 +20,14 @@ namespace CatanersTest
             Assert.AreEqual(1, et.playerTurn);
             Assert.AreEqual(EndTurn.Phase.GamePhase, et.phase);
         }
+
+        [Test]
+        public void testToJson()
+        {
+            // Really this time
+            EndTurn et = new EndTurn(1, EndTurn.Phase.GamePhase);
+
+            Assert.AreEqual("{\"playerTurn\":1,\"phase\":1}", et.toJson());
+        }
     }
 }
