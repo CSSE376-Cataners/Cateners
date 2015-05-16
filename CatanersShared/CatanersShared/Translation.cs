024481755bbedaf1e3d1ce7b3ceead9319acdaa7
Trottasn <trottasn@rose-hulman.edu>
2015-05-16 17:50:26 -0400

    Merge Fix

--- CatanersShared/CatanersShared/Translation.cs
+++ CatanersShared/CatanersShared/Translation.cs
@@ -20,11 +20,7 @@ namespace CatanersShared
             NewLongestRoad
         };
 
-<<<<<<< HEAD
         public enum DevelopmentType { Buy, Knight, Monopoly, RoadBuilding, YearOfPlenty, VictoryPoint, NA }
-=======
-        public enum DevelopmentType { Buy, NA, Knight, Monopoly, RoadBuilding, YearOfPlenty, VictoryPoint }
->>>>>>> origin/master
 
         public static int[][] jsonToIntArrayTwo(string s) 
         {
