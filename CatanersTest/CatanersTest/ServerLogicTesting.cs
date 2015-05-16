8c04bc9663547a9cb1f815feb5fc0ccf63784a9e
Trent Punt <tpunt3@gmail.com>
2015-05-16 16:43:19 -0400

    wrote code so that adding settlement after startphase adds VP TDD

--- CatanersTest/CatanersTest/ServerLogicTesting.cs
+++ CatanersTest/CatanersTest/ServerLogicTesting.cs
@@ -186,7 +186,7 @@ namespace CatanersTest
         }
 
         [Test]
-        public void testAddingSettlementGivesVP()
+        public void testAddingSettlementGivesVPOnStartPhase()
         {
             Player p0 = new Player("Michael Jordan");
             Lobby lobby = new Lobby("Basketball", 100, p0, 10);
@@ -198,6 +198,30 @@ namespace CatanersTest
             logic.determineSettlementAvailability(p0.Username,1);
             Assert.AreEqual(1, logic.gameLobby.gamePlayers[0].victoryPoints);
         }
+
+        [Test]
+        public void testAddingSettlementGivesVPAfterStartPhase()
+        {
+            Player p0 = new Player("Michael Jordan");
+            Lobby lobby = new Lobby("Basketball", 100, p0, 10);
+            lobby.addPlayer(new Player("p1"));
+            lobby.addPlayer(new Player("p2"));
+            lobby.addPlayer(new Player("p3"));
+            ServerLogic logic = new ServerLogic(lobby);
+            logic.isStartPhase1 = false;
+            logic.isStartPhase2 = false;
+            logic.setSettlementActivity(15, p0.Username);
+            logic.setSettlementActivity(23, p0.Username);
+            logic.gameLobby.gamePlayers[0].resources[Resource.TYPE.Brick] = 1;
+            logic.gameLobby.gamePlayers[0].resources[Resource.TYPE.Wheat] = 1;
+            logic.gameLobby.gamePlayers[0].resources[Resource.TYPE.Wood] = 1;
+            logic.gameLobby.gamePlayers[0].resources[Resource.TYPE.Sheep] = 1;
+            logic.setRoadActivity(2, p0.Username);
+
+            logic.determineSettlementAvailability(p0.Username, 1);
+            Assert.AreEqual(1, logic.gameLobby.gamePlayers[0].victoryPoints);
+        }
+
 
 
         /*public void TestResourceConstructor()
