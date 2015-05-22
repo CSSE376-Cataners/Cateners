8fc8cfa17bf061f754b888669a1e75711e12d522
cjm721 <cjmiller721@gmail.com>
2015-05-22 15:31:10 -0400

    New Version and end-all catch for exceptions

--- CatanersServer/CatenersServer/Client.cs
+++ CatanersServer/CatenersServer/Client.cs
@@ -71,7 +71,7 @@ namespace CatenersServer
                     thread.Start();
 
                 }
-                catch (System.IO.IOException)
+                catch
                 {
                     // Client caused exception just disconnect.
                     this.reader.Close();
@@ -94,84 +94,88 @@ namespace CatenersServer
         private object processMessageLock = false;
         public void processesMessage(String s)
         {
-            lock (processMessageLock)
+            try
             {
-            Message msg = Message.fromJson(s);
-            System.Diagnostics.Debug.WriteLine(String.Format("[{0}] {1}::TYPE: {2} :: Message: {3}", DateTime.Now.ToString("T"), this.userName, msg.type, msg.message));
-
-
-            // TODO Pick all messages that would be considered turn based
-            
-                if (this.gameLobby != null && this.serverLogic != null && !this.gameLobby.gamePlayers[serverLogic.playerTurn].Username.Equals(this.userName))
-                if (!Data.DEBUG && turnRequired.Contains(msg.type))
+                lock (processMessageLock)
                 {
-                    return;
+                    Message msg = Message.fromJson(s);
+                    System.Diagnostics.Debug.WriteLine(String.Format("[{0}] {1}::TYPE: {2} :: Message: {3}", DateTime.Now.ToString("T"), this.userName, msg.type, msg.message));
+
+
+                    // TODO Pick all messages that would be considered turn based
+
+                    if (this.gameLobby != null && this.serverLogic != null && !this.gameLobby.gamePlayers[serverLogic.playerTurn].Username.Equals(this.userName))
+                        if (!Data.DEBUG && turnRequired.Contains(msg.type))
+                        {
+                            return;
+                        }
+
+                    switch (msg.type)
+                    {
+                        case Translation.TYPE.Login:
+                            PM_Login(msg);
+                            break;
+                        case Translation.TYPE.Register:
+                            PM_Register(msg);
+                            break;
+                        case Translation.TYPE.RequestLobbies:
+                            PM_RequestLobbies(msg);
+                            break;
+                        case Translation.TYPE.BuySettlement:
+                            PM_BuySettlement(msg);
+                            break;
+                        case Translation.TYPE.BuyRoad:
+                            PM_BuyRoad(msg);
+                            break;
+                        case Translation.TYPE.CreateLobby:
+                            PM_CreateLobby(msg);
+                            break;
+                        case Translation.TYPE.ChangeReadyStatus:
+                            PM_ChangeReadyStatus(msg);
+                            break;
+                        case Translation.TYPE.JoinLobby:
+                            PM_JoinLobby(msg);
+                            break;
+                        case Translation.TYPE.LeaveLobby:
+                            PM_LeaveLobby(msg);
+                            break;
+                        case Translation.TYPE.UpdateLobby:
+                            PM_UpdateLobby(msg);
+                            break;
+                        case Translation.TYPE.RegenerateBoard:
+                            PM_RegenerateBoard(msg);
+                            break;
+                        case Translation.TYPE.GetGameLobby:
+                            PM_GetGameLobby(msg);
+                            break;
+                        case Translation.TYPE.StartGame:
+                            PM_StartGame(msg);
+                            break;
+                        case Translation.TYPE.StartTrade:
+                            PM_StartTrade(msg);
+                            break;
+                        case Translation.TYPE.Chat:
+                            PM_Chat(msg);
+                            break;
+                        case Translation.TYPE.TradeResponce:
+                            PM_TradeResponse(msg);
+                            break;
+                        case Translation.TYPE.EndTurn:
+                            PM_EndTurn(msg);
+                            break;
+                        case Translation.TYPE.DiceRoll:
+                            PM_DiceRoll(msg);
+                            break;
+                        case Translation.TYPE.DevelopmentCard:
+                            PM_DevelopmentCard(msg);
+                            break;
+                        default:
+                            // We Are just going to ignore it.
+                            break;
+                    }
                 }
-            
-                switch (msg.type)
-                {
-                case Translation.TYPE.Login:
-                    PM_Login(msg);
-                break;
-                case Translation.TYPE.Register:
-                    PM_Register(msg);
-                break;
-                case Translation.TYPE.RequestLobbies:
-                    PM_RequestLobbies(msg);
-                break;
-                case Translation.TYPE.BuySettlement:
-                    PM_BuySettlement(msg);
-                break;
-                case Translation.TYPE.BuyRoad:
-                    PM_BuyRoad(msg);
-                break;
-                    case Translation.TYPE.CreateLobby:
-                    PM_CreateLobby(msg);
-                break;
-                case Translation.TYPE.ChangeReadyStatus:
-                    PM_ChangeReadyStatus(msg);
-                break;
-                case Translation.TYPE.JoinLobby:
-                    PM_JoinLobby(msg);
-                break;
-                case Translation.TYPE.LeaveLobby:
-                    PM_LeaveLobby(msg);
-                break;
-                case Translation.TYPE.UpdateLobby:
-                    PM_UpdateLobby(msg);
-                break;
-                case Translation.TYPE.RegenerateBoard:
-                    PM_RegenerateBoard(msg);
-                break;
-                case Translation.TYPE.GetGameLobby:
-                    PM_GetGameLobby(msg);
-                break;
-                case Translation.TYPE.StartGame:
-                    PM_StartGame(msg);
-                break;
-                case Translation.TYPE.StartTrade:
-                    PM_StartTrade(msg);
-                break;
-                case Translation.TYPE.Chat:
-                    PM_Chat(msg);
-                break;
-                case Translation.TYPE.TradeResponce:
-                    PM_TradeResponse(msg);
-                break;
-                case Translation.TYPE.EndTurn:
-                    PM_EndTurn(msg);
-                break;
-                case Translation.TYPE.DiceRoll:
-                    PM_DiceRoll(msg);
-                break;
-                case Translation.TYPE.DevelopmentCard:
-                    PM_DevelopmentCard(msg);
-                    break;
-                default:
-                    // We Are just going to ignore it.
-                break;
             }
-        }
+            catch { }
         }
 
 
